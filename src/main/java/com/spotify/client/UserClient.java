a4e02f61a08bdcf2e8ab6b9f48c891984daa17ec
AutomationWarrior <warriorautmation@gmail.com>
2023-12-26 17:49:01 +0700

    Commit new client api

--- src/main/java/com/spotify/client/UserClient.java
+++ src/main/java/com/spotify/client/UserClient.java
@@ -1,4 +1,15 @@
 package com.spotify.client;
 
+import com.spotify.dto.Playlist.Playlist;
+import io.restassured.response.Response;
+
+import static com.spotify.route.Route.PLAYLIST;
+import static com.spotify.route.Route.USERS;
+import static com.spotify.tokenManager.TokenManager.getAccessToken;
+
 public class UserClient {
+
+    public static Response createUser(Playlist requestPlaylist){
+        return SimpleClient.post(USERS+"/31tajp4w6u4syg4evc4glc7qsubi" + PLAYLIST, getAccessToken(), requestPlaylist);
+    }
 }
